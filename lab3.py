cdf1c02c4d12263d0364c47187053e7aa9154369
iamstacyok <iamstacyok@gmail.com>
2025-10-08 17:32:58 +0500

    App created

---
+++ lab3.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from transformers import pipeline
+import torch
+
+st.title("Работа с языковой моделью")
+
+prompt = st.text_area("Промпт", "Сгенерируй описание для дисциплины «Программная инженерия» в магистратуре ТюмГУ")
+max_length = st.slider("Максимум токенов", 50, 1024, 300)
+
+if st.button("Запустить"):
+    with st.spinner("Загрузка модели..."):
+        device = 0 if torch.cuda.is_available() else -1
+        generator = pipeline("text-generation", model="openai-community/gpt2-medium", device=device) #пришлость заменить модель, предыдущая слишком много весила...
+        out = generator(prompt, max_new_tokens=max_length, do_sample=True, temperature=0.7)
+        st.write(out[0]["generated_text"])
