a8fe1829edbb033b80e42324d1145686aff807af
iamstacyok <iamstacyok@gmail.com>
2025-10-08 17:51:14 +0500

    Updated model

--- lab3.py
+++ lab3.py
@@ -10,6 +10,6 @@ max_length = st.slider("Максимум токенов", 50, 1024, 300)
 if st.button("Запустить"):
     with st.spinner("Загрузка модели..."):
         device = 0 if torch.cuda.is_available() else -1
-        generator = pipeline("text-generation", model="openai-community/gpt2-medium", device=device) #пришлость заменить модель, предыдущая слишком много весила...
+        generator = pipeline("text-generation", model="google/gemma-3-270m", device=device)
         out = generator(prompt, max_new_tokens=max_length, do_sample=True, temperature=0.7)
         st.write(out[0]["generated_text"])
